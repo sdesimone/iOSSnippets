777bfc984c247e968fe9e51480a9901435c0f4f7
Sergio De Simone <sdesimone@gmx.net>
2012-06-15 09:08:16 +0200

    added CALayer+SDSContentsAnimation.m a category to easily handle png sequences as animation

---
+++ SDSCategories/CALayer+SDSContentsAnimation.m
@@ -0,0 +1,51 @@
+//
+//  CALayer+SDSContentsAnimation.m
+//  MantraPhoneTest
+//
+//  Created by sergio on 5/2/12.
+//  Copyright 2012 Sergio De Simone, Freescapes Labs. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+@implementation CALayer (SDSContentsAnimation)
+
+//////////////////////////////////////////////////////////////////////////////////////////////
++ (void)swizzleMethod:(SEL)originalSel andMethod:(SEL)swizzledSel {
+    
+    Method original = class_getInstanceMethod(self, originalSel);
+    Method swizzled = class_getInstanceMethod(self, swizzledSel);
+    if (original && swizzled)
+        method_exchangeImplementations(original, swizzled);
+    else
+        NSLog(@"CALayer+SDSContentsAnimation Swizzling Fault: methods not found.");
+
+}    
+
+//////////////////////////////////////////////////////////////////////////////////////////////
++ (void)load {
+    [self swizzleMethod:@selector(setContents:) andMethod:@selector(setContentsSwizzled:)];
+    
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+- (void)setContentsSwizzled:(id)contents {
+    if ([contents isKindOfClass:[NSArray class]]) {
+        
+        CAKeyframeAnimation* animation = [CAKeyframeAnimation animationWithKeyPath:@"contents"];
+        [animation setCalculationMode:kCAAnimationLinear];
+        [animation setDuration:contentsAnimationDuration];
+        [animation setRepeatCount:HUGE_VALF];
+        [animation setValues:contents];        
+        [self addAnimation:animation forKey:@"contents"];
+        
+    } else
+        [self setContentsSwizzled:contents];
+}
+
+@end
+
+
